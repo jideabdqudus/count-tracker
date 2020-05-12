2b3caebbc1215a7d4daef5a66d35aeafb4c88676
jideabdqudus <jideabdqudus@gmail.com>
2020-05-13 00:01:08 +0100

    Returned to regular state

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import React, {useState} from 'react';
 import './App.css';
 
 const App =()=> {
-  countTracker(
-     <h1>How ar you</h1>;
-  )
+  
   return (
     <div className="App">
-      <h1>Count Tracker</h1>
-      <div>{countTracker}</div>
+      
     </div>
   );
 }
