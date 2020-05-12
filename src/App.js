2e1fe97c798e8879eee05a0c19dc9c59a3ef823c
jideabdqudus <jideabdqudus@gmail.com>
2020-05-13 00:00:41 +0100

    countTracker function error

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React, {useState} from 'react';
 import './App.css';
 
 const App =()=> {
+  countTracker(
+     <h1>How ar you</h1>;
+  )
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Count Tracker</h1>
+      <div>{countTracker}</div>
     </div>
   );
 }
 
 export default App;
+
+
+//Added useState Hooks
