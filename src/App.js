db83f59bed24be5f7f0f38c7c9719ec67d301860
jideabdqudus <jideabdqudus@gmail.com>
2020-05-13 02:18:30 +0100

    Added Commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-const App =()=> {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const [count, addCount] = useState(0);
   return (
     <div className="App">
-        <h1>Count-Tracker</h1>
-            
+      <h1>Count-Tracker</h1>
+      <div>Button was clicked {count} times</div>
+      <button style={buttonStyles} onClick={() => addCount(count + 1)}>Add</button>
+      <button onClick={() => addCount(count - 1)}>Subtract</button>
     </div>
   );
+};
+
+const buttonStyles =() =>{
+  
 }
 
 export default App;
 
-
 //1. Added useState Hooks
+//2. initial count state of 0
+//3. setState of addCount
+//4. Initialize buttons with count functions
