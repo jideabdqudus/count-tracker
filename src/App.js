88ea27fbd20a5820a91f3d630b4424c77985eb8c
jideabdqudus <jideabdqudus@gmail.com>
2020-05-13 13:07:13 +0100

    Final Count Tracker Commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./tailwind.generated.css";
 const App = () => {
   const [count, addCount] = useState(0);
   return (
-    <div className="App max-w-sm rounded overflow-hidden shadow-lg">
+    <div className="App max-w-sm rounded overflow-hidden shadow-lg container">
       <div className=" px-6 py-4">
         <h1 className="font-bold text-xl mb-2">Count-Tracker</h1>
         <div className="text-gray-700 text-base">
