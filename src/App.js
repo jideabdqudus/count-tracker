9b710f594ab0bb47401162c7bce68624653ac9d5
jideabdqudus <jideabdqudus@gmail.com>
2020-05-13 01:34:33 +0100

    Added Commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, {useState} from 'react';
 import './App.css';
 
 const App =()=> {
+
+  const [count, setCount] = useState(0)
   
   return (
     <div className="App">
-        <h1>Hello World</h1>     
+        <h1>Hello World</h1>
+        {setCount}     
     </div>
   );
 }
